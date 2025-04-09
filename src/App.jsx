2817035bda8a08473c0fd11e0109e0479e833605
Kendaka <barengkendrick4@gmail.com>
2025-04-09 21:34:45 +0400

    ternary operator update

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ function App() {
 
   const name = "Kendrick"
   const luckyNumber = Math.floor(Math.random() * 10) + 1
+  const isLuckyNumber = luckyNumber >= 6 ? "Greater than" : "Less than"
 
   return (
     <div>
       <h1>{name}</h1>
-      <p>Your lucky number is {luckyNumber}.</p>
+      <p>Your lucky number is {isLuckyNumber}.</p>
     </div>
   )
 }
