94ddc3d2234bd3ddad9f70ab4b2bf01c9607b5f6
Kendaka <barengkendrick4@gmail.com>
2025-04-11 00:57:39 +0400

    put it back to amber-normal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
 
     return(
         <div className='bg-slate-700 flex items-start justify-start py-8 px-14'>
-            <h1 className='font-serif text-5xl text-amber-200'>{name}</h1>
+            <h1 className='font-serif text-5xl text-amber-50'>{name}</h1>
         </div>
     )
 }
