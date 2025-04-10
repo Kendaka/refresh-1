75bd1ee64be098cb3d2fb47cd1950f2b9b9e48ae
Kendaka <barengkendrick4@gmail.com>
2025-04-11 00:56:35 +0400

    small changes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,7 @@
     const Note = () => {
         return(
             <div className='mx-auto my-8 max-w-md'> 
-                <div className='border border-slate-300 rounded-lg p-6 bg-amber-50'>
+                <div className='border border-slate-300 rounded-lg p-8 bg-amber-50'>
                     <h1 className="text-lg font-bold text-slate-700">Note</h1>
                     <p className="text-sm text-slate-500">This is a note component.</p>
                 </div>
