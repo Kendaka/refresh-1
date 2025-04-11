71a78823bf98e21c4a2f843df0b0779a22894640
Kendaka <barengkendrick4@gmail.com>
2025-04-11 13:05:39 +0400

    dynamic note 2nd initialization

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Note from './components/Note'
 import Footer from './components/Footer'
+import notes from './notes'
 import './App.css'
 
 function App() {
@@ -9,7 +10,11 @@ function App() {
     <div className='bg-slate-100 min-h-screen flex flex-col'>
       <Header />
       <div className='flex-grow'>
-        <Note />
+        <Note
+          key={notes.map(note => note.key)}
+          title={notes.map(note => note.title)}
+          content={notes.map(note => note.content)}
+        />
       </div>
       <Footer />
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
     import React from 'react'
 
-    const Note = () => {
+    const Note = (props) => {
         return(
             <div className='mx-auto my-8 max-w-md'> 
                 <div className='border border-slate-300 rounded-lg p-8 bg-amber-50'>
-                    <h1 className="text-lg font-bold text-slate-700">Note</h1>
-                    <p className="text-sm text-slate-500">This is a note component.</p>
+                    <h1 className="text-lg font-bold text-slate-700">{props.title}</h1>
+                    <p className="text-sm text-slate-500">{props.content}</p>
                 </div>
             </div>
         )
