353d9b40f9b8fd679611101024d0f7d59f649c70
Kendaka <barengkendrick4@gmail.com>
2025-04-11 13:51:03 +0400

    adjust the height of each note

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,16 @@ import Footer from './components/Footer'
 import notes from './notes'
 import './App.css'
 
-const createNotes = (note) => {
-  return (
-    <Note
-      key={note.key}
-      title={note.title}
-      content={note.content}
-    />
-  )
-}
-
 function App() {
   return ( 
     <div className='bg-slate-100 min-h-screen flex flex-col'>
       <Header />
-      <div className='flex-grow p-4 flex flex-wrap justify-center gap-4'>
-        {notes.map(createNotes)}
+      <div className='flex-grow p-4 grid grid-cols-4 gap-4'>
+        {notes.map((noteItem) => {
+          return (
+            <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />
+          )
+        })}
       </div>
       <Footer />
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Note = (props) => {
-    return(
-        <div className='w-64'> 
-            <div className='border border-slate-300 rounded-lg p-4 bg-amber-50 h-64 flex flex-col'>
-                <h1 className="text-lg font-bold text-slate-700 mb-2 line-clamp-1">{props.title}</h1>
-                <p className="text-sm text-slate-500 line-clamp-4 flex-grow">{props.content}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className='h-full p-4'> 
+      <div className='border border-slate-300 rounded-lg p-4 bg-amber-50 h-full flex flex-col max-h-60 overflow-y-auto'>
+        <h1 className="text-2xl font-bold text-slate-700 mb-2">{props.title}</h1>
+        <p className="text-lg text-slate-500">{props.content}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Note
