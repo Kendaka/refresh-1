b785be53b187bed419d2b81b4b1d554fa10e69ac
Kendaka <barengkendrick4@gmail.com>
2025-04-12 21:42:39 +0400

    add the props.content

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,7 +5,7 @@ const Note = (props) => {
     <div className='h-full p-2'> 
       <div className='border border-slate-300 rounded-lg p-4 bg-amber-50 h-full flex flex-col max-h-60 overflow-y-auto'>
         <h1 className="text-2xl font-bold text-slate-700 mb-2">{props.title}</h1>
-        <p className="text-lg text-slate-500"> {props.content}</p>
+        <p className="text-lg text-slate-500">{props.content}</p>
       </div>
     </div>
   )
