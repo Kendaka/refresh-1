46301e4e6b29e7814ea5c5efff41be70bbae32c9
Kendaka <barengkendrick4@gmail.com>
2025-04-12 21:41:40 +0400

    props title added

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ const Note = (props) => {
   return (
     <div className='h-full p-2'> 
       <div className='border border-slate-300 rounded-lg p-4 bg-amber-50 h-full flex flex-col max-h-60 overflow-y-auto'>
-        <h1 className="text-2xl font-bold text-slate-700 mb-2">{props.title}</h1>
+        <h1 className="text-2xl font-bold text-slate-700 mb-2"> {props.title}</h1>
         <p className="text-lg text-slate-500">{props.content}</p>
       </div>
     </div>
