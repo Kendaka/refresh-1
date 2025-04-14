939e15f9067c17aefd6a7c04298bffe7b52b7e81
Kendaka <barengkendrick4@gmail.com>
2025-04-14 21:45:12 +0400

    added a handle change for two inputs in createArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+
+        setNote((prevNote) => {
+            return{
+                ...prevNote,
+                [name] : value
+            }
+        })
+    }
   return (
     <div className="bg-slate-100 p-8 flex justify-center">
       <form className="bg-amber-50 w-full max-w-md p-4 rounded-lg shadow-md relative">
@@ -8,12 +24,16 @@ function CreateArea() {
           name="title"
           placeholder="Title"
           className="w-full p-2 mb-4 text-lg font-medium text-slate-700 bg-transparent border-b border-slate-300 focus:outline-none focus:border-amber-400"
+          value={note.title}
+          onChange={handleChange}
         />
         <textarea
           name="content"
           placeholder="Take a note..."
           rows="3"
           className="w-full p-2 text-slate-600 bg-transparent focus:outline-none resize-none"
+          value={note.content}
+          onChange={handleChange}
         />
         <button
           type="submit"
